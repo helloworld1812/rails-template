93e563792cbd13a5d0d1660946b7ebdbd2a5c9b0
Ryan Lv <xiaorong.ruby@gmail.com>
2021-03-29 19:06:08 +0800

    update

--- db/schema.rb
+++ db/schema.rb
@@ -16,48 +16,33 @@ ActiveRecord::Schema.define(version: 2021_03_26_051340) do
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
 
-  create_table "companies", force: :cascade do |t|
-    t.uuid "uuid", null: false
-    t.string "name"
-    t.integer "status"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["uuid"], name: "index_companies_on_uuid"
+  create_table "connector_example_models", primary_key: "uuid", id: :uuid, default: nil, force: :cascade do |t|
+    t.datetime "core_created_at", precision: 6
+    t.datetime "core_updated_at", precision: 6
+    t.datetime "core_deleted_at", precision: 6
   end
 
-  create_table "connector_example_models", id: false, force: :cascade do |t|
-    t.uuid "uuid", default: -> { "gen_random_uuid()" }
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.datetime "deleted_at"
-    t.index ["uuid"], name: "index_connector_example_models_on_uuid"
-  end
-
-  create_table "core_companies", id: false, force: :cascade do |t|
-    t.uuid "uuid", default: -> { "gen_random_uuid()" }
+  create_table "core_companies", primary_key: "uuid", id: :uuid, default: nil, force: :cascade do |t|
     t.string "status"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.datetime "deleted_at"
-    t.index ["uuid"], name: "index_core_companies_on_uuid"
+    t.string "name"
+    t.datetime "core_created_at", precision: 6
+    t.datetime "core_updated_at", precision: 6
+    t.datetime "core_deleted_at", precision: 6
   end
 
-  create_table "core_configurations", id: false, force: :cascade do |t|
-    t.uuid "uuid", default: -> { "gen_random_uuid()" }
+  create_table "core_configurations", primary_key: "uuid", id: :uuid, default: nil, force: :cascade do |t|
     t.uuid "core_company_uuid", null: false
     t.string "status"
     t.jsonb "config", default: {}
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.datetime "deleted_at"
+    t.datetime "core_created_at", precision: 6
+    t.datetime "core_updated_at", precision: 6
+    t.datetime "core_deleted_at", precision: 6
     t.index ["core_company_uuid"], name: "index_core_configurations_on_core_company_uuid"
-    t.index ["uuid"], name: "index_core_configurations_on_uuid"
   end
 
-  create_table "core_employee_informations", id: false, force: :cascade do |t|
-    t.uuid "uuid", default: -> { "gen_random_uuid()" }
-    t.uuid "employee_uuid"
-    t.uuid "company_uuid"
+  create_table "core_employee_informations", primary_key: "uuid", id: :uuid, default: nil, force: :cascade do |t|
+    t.uuid "core_employee_uuid", null: false
+    t.uuid "core_company_uuid", null: false
     t.string "first_name"
     t.string "middle_name"
     t.string "last_name"
@@ -82,53 +67,45 @@ ActiveRecord::Schema.define(version: 2021_03_26_051340) do
     t.date "termination_date"
     t.string "job_title"
     t.string "department_name"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.datetime "deleted_at"
-    t.index ["company_uuid"], name: "index_core_employee_informations_on_company_uuid"
-    t.index ["employee_uuid"], name: "index_core_employee_informations_on_employee_uuid"
-    t.index ["uuid"], name: "index_core_employee_informations_on_uuid"
+    t.datetime "core_created_at", precision: 6
+    t.datetime "core_updated_at", precision: 6
+    t.datetime "core_deleted_at", precision: 6
+    t.index ["core_company_uuid"], name: "index_core_ei_on_company_uuid"
+    t.index ["core_employee_uuid"], name: "index_core_ei_on_employee_uuid"
   end
 
-  create_table "core_employee_onboarding_processes", id: false, force: :cascade do |t|
-    t.uuid "uuid", default: -> { "gen_random_uuid()" }
-    t.uuid "core_company_uuid"
-    t.uuid "core_employee_uuid"
-    t.jsonb "configurations"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.datetime "deleted_at"
-    t.index ["core_company_uuid"], name: "index_core_employee_onboarding_processes_on_core_company_uuid"
-    t.index ["core_employee_uuid"], name: "index_core_employee_onboarding_processes_on_core_employee_uuid"
-    t.index ["uuid"], name: "index_core_employee_onboarding_processes_on_uuid"
-  end
-
-  create_table "core_employees", id: false, force: :cascade do |t|
-    t.uuid "uuid", default: -> { "gen_random_uuid()" }
+  create_table "core_employee_onboarding_processes", primary_key: "uuid", id: :uuid, default: nil, force: :cascade do |t|
+    t.uuid "core_employee_uuid", null: false
     t.uuid "core_company_uuid", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.datetime "deleted_at"
-    t.index ["core_company_uuid"], name: "index_core_employees_on_core_company_uuid"
-    t.index ["uuid"], name: "index_core_employees_on_uuid"
+    t.jsonb "configurations"
+    t.datetime "core_created_at", precision: 6
+    t.datetime "core_updated_at", precision: 6
+    t.datetime "core_deleted_at", precision: 6
+    t.index ["core_company_uuid"], name: "index_core_eop_on_core_company_uuid"
+    t.index ["core_employee_uuid"], name: "index_core_eop_on_core_employee_uuid"
   end
 
-  create_table "core_hrs", id: false, force: :cascade do |t|
-    t.uuid "uuid", default: -> { "gen_random_uuid()" }
+  create_table "core_employees", primary_key: "uuid", id: :uuid, default: nil, force: :cascade do |t|
+    t.uuid "core_company_uuid", null: false
+    t.datetime "core_created_at", precision: 6
+    t.datetime "core_updated_at", precision: 6
+    t.datetime "core_deleted_at", precision: 6
+    t.index ["core_company_uuid"], name: "index_core_employees_on_core_company_uuid"
+  end
+
+  create_table "core_hrs", primary_key: "uuid", id: :uuid, default: nil, force: :cascade do |t|
     t.uuid "core_company_uuid", null: false
     t.string "phone"
     t.string "name"
     t.string "email"
     t.string "status"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.datetime "deleted_at"
+    t.datetime "core_created_at", precision: 6
+    t.datetime "core_updated_at", precision: 6
+    t.datetime "core_deleted_at", precision: 6
     t.index ["core_company_uuid"], name: "index_core_hrs_on_core_company_uuid"
-    t.index ["uuid"], name: "index_core_hrs_on_uuid"
   end
 
-  create_table "core_tasks", id: false, force: :cascade do |t|
-    t.uuid "uuid", default: -> { "gen_random_uuid()" }
+  create_table "core_tasks", primary_key: "uuid", id: :uuid, default: nil, force: :cascade do |t|
     t.uuid "core_employee_uuid", null: false
     t.uuid "core_configuration_uuid", null: false
     t.uuid "core_process_uuid", null: false
@@ -139,39 +116,13 @@ ActiveRecord::Schema.define(version: 2021_03_26_051340) do
     t.string "role", null: false
     t.string "status"
     t.string "complete_return_url"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.datetime "deleted_at"
+    t.datetime "core_created_at", precision: 6
+    t.datetime "core_updated_at", precision: 6
+    t.datetime "core_deleted_at", precision: 6
     t.index ["core_company_uuid"], name: "index_core_tasks_on_core_company_uuid"
     t.index ["core_configuration_uuid"], name: "index_core_tasks_on_core_configuration_uuid"
     t.index ["core_employee_uuid"], name: "index_core_tasks_on_core_employee_uuid"
     t.index ["core_process_uuid"], name: "index_core_tasks_on_core_process_uuid"
-    t.index ["uuid"], name: "index_core_tasks_on_uuid"
-  end
-
-  create_table "employees", force: :cascade do |t|
-    t.uuid "uuid", null: false
-    t.bigint "company_id"
-    t.bigint "hr_id"
-    t.integer "status"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["company_id"], name: "index_employees_on_company_id"
-    t.index ["hr_id"], name: "index_employees_on_hr_id"
-    t.index ["uuid"], name: "index_employees_on_uuid"
-  end
-
-  create_table "hrs", force: :cascade do |t|
-    t.uuid "uuid", null: false
-    t.bigint "company_id"
-    t.string "phone"
-    t.string "name"
-    t.string "email"
-    t.integer "status"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["company_id"], name: "index_hrs_on_company_id"
-    t.index ["uuid"], name: "index_hrs_on_uuid"
   end
 
 end
