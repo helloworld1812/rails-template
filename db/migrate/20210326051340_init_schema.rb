1b33a64105e0b004c4ed82cd6ab22cb1025d250e
Ryan Lv <xiaorong.ruby@gmail.com>
2021-03-29 19:04:45 +0800

    update

--- db/migrate/20210326051340_init_schema.rb
+++ db/migrate/20210326051340_init_schema.rb
@@ -5,35 +5,33 @@ class InitSchema < ActiveRecord::Migration[6.1]
     enable_extension "plpgsql"
 
     create_table "connector_example_models", id: false do |t|
-      t.uuid "uuid", default: -> { "gen_random_uuid()" }
-      t.datetime "created_at", precision: 6, null: false
-      t.datetime "updated_at", precision: 6, null: false
-      t.datetime "deleted_at"
-      t.index ["uuid"], name: "index_connector_example_models_on_uuid"
+      t.uuid "uuid", null: false, primary_key: true
+      t.datetime "core_created_at", precision: 6
+      t.datetime "core_updated_at", precision: 6
+      t.datetime "core_deleted_at", precision: 6
     end
     create_table "core_companies", id: false do |t|
-      t.uuid "uuid", default: -> { "gen_random_uuid()" }
+      t.uuid "uuid", null: false, primary_key: true
       t.string "status"
-      t.datetime "created_at", precision: 6, null: false
-      t.datetime "updated_at", precision: 6, null: false
-      t.datetime "deleted_at"
-      t.index ["uuid"], name: "index_core_companies_on_uuid"
+      t.string "name"
+      t.datetime "core_created_at", precision: 6
+      t.datetime "core_updated_at", precision: 6
+      t.datetime "core_deleted_at", precision: 6
     end
     create_table "core_configurations", id: false do |t|
-      t.uuid "uuid", default: -> { "gen_random_uuid()" }
+      t.uuid "uuid", null: false, primary_key: true
       t.uuid "core_company_uuid", null: false
       t.string "status"
       t.jsonb "config", default: {}
-      t.datetime "created_at", precision: 6, null: false
-      t.datetime "updated_at", precision: 6, null: false
-      t.datetime "deleted_at"
+      t.datetime "core_created_at", precision: 6
+      t.datetime "core_updated_at", precision: 6
+      t.datetime "core_deleted_at", precision: 6
       t.index ["core_company_uuid"], name: "index_core_configurations_on_core_company_uuid"
-      t.index ["uuid"], name: "index_core_configurations_on_uuid"
     end
     create_table "core_employee_informations", id: false do |t|
-      t.uuid "uuid", default: -> { "gen_random_uuid()" }
-      t.uuid "employee_uuid"
-      t.uuid "company_uuid"
+      t.uuid "uuid", null: false, primary_key: true
+      t.uuid "coreemployee_uuid", null: false
+      t.uuid "core_company_uuid", null: false
       t.string "first_name"
       t.string "middle_name"
       t.string "last_name"
@@ -58,49 +56,48 @@ class InitSchema < ActiveRecord::Migration[6.1]
       t.date "termination_date"
       t.string "job_title"
       t.string "department_name"
-      t.datetime "created_at", precision: 6, null: false
-      t.datetime "updated_at", precision: 6, null: false
-      t.datetime "deleted_at"
-      t.index ["company_uuid"], name: "index_core_employee_informations_on_company_uuid"
-      t.index ["employee_uuid"], name: "index_core_employee_informations_on_employee_uuid"
-      t.index ["uuid"], name: "index_core_employee_informations_on_uuid"
+      t.datetime "core_created_at", precision: 6
+      t.datetime "core_updated_at", precision: 6
+      t.datetime "core_deleted_at", precision: 6
+      t.index ["core_company_uuid"], name: "index_core_ei_on_company_uuid"
+      t.index ["core_employee_uuid"], name: "index_core_ei_on_employee_uuid"
+      # t.index ["uuid"], name: "index_core_employee_informations_on_uuid"
     end
     create_table "core_employee_onboarding_processes", id: false do |t|
-      t.uuid "uuid", default: -> { "gen_random_uuid()" }
-      t.uuid "core_company_uuid"
-      t.uuid "core_employee_uuid"
+      t.uuid "uuid", null: false, primary_key: true
+      t.uuid "core_employee_uuid", null: false
+      t.uuid "core_company_uuid", null: false
       t.jsonb "configurations"
-      t.datetime "created_at", precision: 6, null: false
-      t.datetime "updated_at", precision: 6, null: false
-      t.datetime "deleted_at"
-      t.index ["core_company_uuid"], name: "index_core_employee_onboarding_processes_on_core_company_uuid"
-      t.index ["core_employee_uuid"], name: "index_core_employee_onboarding_processes_on_core_employee_uuid"
-      t.index ["uuid"], name: "index_core_employee_onboarding_processes_on_uuid"
+      t.datetime "core_created_at", precision: 6
+      t.datetime "core_updated_at", precision: 6
+      t.datetime "core_deleted_at", precision: 6
+      t.index ["core_company_uuid"], name: "index_core_eop_on_core_company_uuid"
+      t.index ["core_employee_uuid"], name: "index_core_eop_on_core_employee_uuid"
     end
     create_table "core_employees", id: false do |t|
-      t.uuid "uuid", default: -> { "gen_random_uuid()" }
+      t.uuid "uuid", null: false, primary_key: true
       t.uuid "core_company_uuid", null: false
-      t.datetime "created_at", precision: 6, null: false
-      t.datetime "updated_at", precision: 6, null: false
-      t.datetime "deleted_at"
+      t.datetime "core_created_at", precision: 6
+      t.datetime "core_updated_at", precision: 6
+      t.datetime "core_deleted_at", precision: 6
       t.index ["core_company_uuid"], name: "index_core_employees_on_core_company_uuid"
-      t.index ["uuid"], name: "index_core_employees_on_uuid"
+      # t.index ["uuid"], name: "index_core_employees_on_uuid"
     end
     create_table "core_hrs", id: false do |t|
-      t.uuid "uuid", default: -> { "gen_random_uuid()" }
+      t.uuid "uuid", null: false, primary_key: true
       t.uuid "core_company_uuid", null: false
       t.string "phone"
       t.string "name"
       t.string "email"
       t.string "status"
-      t.datetime "created_at", precision: 6, null: false
-      t.datetime "updated_at", precision: 6, null: false
-      t.datetime "deleted_at"
+      t.datetime "core_created_at", precision: 6
+      t.datetime "core_updated_at", precision: 6
+      t.datetime "core_deleted_at", precision: 6
       t.index ["core_company_uuid"], name: "index_core_hrs_on_core_company_uuid"
-      t.index ["uuid"], name: "index_core_hrs_on_uuid"
+      # t.index ["uuid"], name: "index_core_hrs_on_uuid"
     end
     create_table "core_tasks", id: false do |t|
-      t.uuid "uuid", default: -> { "gen_random_uuid()" }
+      t.uuid "uuid", null: false, primary_key: true
       t.uuid "core_employee_uuid", null: false
       t.uuid "core_configuration_uuid", null: false
       t.uuid "core_process_uuid", null: false
@@ -111,14 +108,14 @@ class InitSchema < ActiveRecord::Migration[6.1]
       t.string "role", null: false
       t.string "status"
       t.string "complete_return_url"
-      t.datetime "created_at", precision: 6, null: false
-      t.datetime "updated_at", precision: 6, null: false
-      t.datetime "deleted_at"
+      t.datetime "core_created_at", precision: 6
+      t.datetime "core_updated_at", precision: 6
+      t.datetime "core_deleted_at", precision: 6
       t.index ["core_company_uuid"], name: "index_core_tasks_on_core_company_uuid"
       t.index ["core_configuration_uuid"], name: "index_core_tasks_on_core_configuration_uuid"
       t.index ["core_employee_uuid"], name: "index_core_tasks_on_core_employee_uuid"
       t.index ["core_process_uuid"], name: "index_core_tasks_on_core_process_uuid"
-      t.index ["uuid"], name: "index_core_tasks_on_uuid"
+      # t.index ["uuid"], name: "index_core_tasks_on_uuid"
     end
   end
 
