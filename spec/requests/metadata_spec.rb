a2585514db6146b19d70ccd57a8327ba0de92a38
Ryan Lv <xiaorong.ruby@gmail.com>
2021-04-12 13:34:03 +0800

    update

--- spec/requests/metadata_spec.rb
+++ spec/requests/metadata_spec.rb
@@ -5,17 +5,17 @@ RSpec.describe "Metadata", type: :request do
     it 'should succeed' do
       get '/metadata'
       expect(response.code).to eq('200')
-
       result = JSON.parse(response.body).with_indifferent_access
+
 
       expect(result[:app_name].present?).to be true
       expect(result[:version].present?).to be true
-      expect(result[:author][:name].present?).to be true
-      expect(result[:author][:email].present?).to be true
+      expect(result[:authors].present?).to be true
       expect(result[:tasks].present?).to be true
+      # expect(result[:host].present?).to be true
 
       result[:tasks].each do |task_name, task_schema|
-        expect(%w[hr employee].include? task_schema['assignee_type']).to be true
+        expect(%w[hr employee].include? task_schema['assignment_category']).to be true
         expect(task_schema[:sequence].present?).to be true
         expect(task_schema[:sequence].is_a?(Integer)).to be true
       end
